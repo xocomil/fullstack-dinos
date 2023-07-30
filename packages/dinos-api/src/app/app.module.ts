8a2239228ce41dfd7f1027a687cf603a66876ea0
xocomil <xocomil@gmail.com>
2023-07-30 12:09:11 -0600

    added `apollo` and `graphql`

--- packages/dinos-api/src/app/app.module.ts
+++ packages/dinos-api/src/app/app.module.ts
@@ -1,10 +1,24 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestGqlModule } from './test-gql/test-gql.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+      include: [TestGqlModule],
+      installSubscriptionHandlers: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      sortSchema: true,
+      // transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
