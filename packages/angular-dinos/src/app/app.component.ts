886d15b321d62e396ff8f88925f0868950c123ba
xocomil <xocomil@gmail.com>
2023-08-10 18:24:39 -0600

    removed test code

--- packages/angular-dinos/src/app/app.component.ts
+++ packages/angular-dinos/src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { JsonPipe } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DinosCrudService } from '@fullstack-dinos/angular-dinos/dinos-gql';
 
 @Component({
   standalone: true,
   imports: [RouterModule, JsonPipe],
   selector: 'dino-root',
-  template: ` <pre>{{ dinos() | json }}</pre>
+  template: ` <h1>Welcome To DinoAPI</h1>
     <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  readonly #dinosCrudService = inject(DinosCrudService);
-  readonly #dinoQuery = this.#dinosCrudService.getAllDinos();
-
-  protected dinos = toSignal(this.#dinoQuery.valueChanges);
-
-  ngOnInit(): void {
-    this.#dinoQuery.startPolling(1000);
-  }
-}
+export class AppComponent {}
