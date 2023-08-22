87c9892e00c270bb3a9d2b1baaf02e7aeaab79bf
xocomil <xocomil@gmail.com>
2023-08-21 18:03:31 -0600

    factored the display template to its own component

--- packages/angular-dinos/dino-ui/src/lib/components/details/details.component.ts
+++ packages/angular-dinos/dino-ui/src/lib/components/details/details.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, Input } from '@angular/core';
 import { DetailsStoreService } from '@fullstack-dinos/angular-dinos/dinos-gql';
+import { DisplayDinoComponent } from '../display-dino/display-dino.component';
 import { EditDinoComponent } from '../edit-dino/edit-dino.component';
 
 @Component({
@@ -12,32 +12,12 @@ import { EditDinoComponent } from '../edit-dino/edit-dino.component';
       *ngIf="detailsStore.editMode(); else displayMode"
     />
     <ng-template #displayMode>
-      <h1 class="mb-1 text-blue-500">{{ detailsStore.dinosaur().name }}</h1>
-      <div class="text-2xl italic text-blue-500 opacity-70">
-        {{ detailsStore.genusSpecies() }}
-      </div>
-      <div class="card card-compact glass mt-4 bg-blue-900 text-blue-50 w-full">
-        <div class="card-body">
-          <h4 class="card-title">Description</h4>
-          <p>
-            <span *ngIf="detailsStore.dinosaur().description; let description">
-              {{ description }}
-            </span>
-            {{ detailsStore.dinosaur().name }} was around
-            {{ detailsStore.dinosaur().heightInMeters }} meters tall and weighed
-            around
-            {{ detailsStore.dinosaur().weightInKilos | number }} kilograms.
-            {{ detailsStore.dinosaur().name }} did
-            {{ detailsStore.dinosaur().hasFeathers ? '' : 'not' }} have
-            feathers.
-          </p>
-        </div>
-      </div>
+      <fullstack-dinos-display-dino />
     </ng-template>
   `,
   styleUrls: ['./details.component.scss'],
   providers: [DetailsStoreService],
-  imports: [CommonModule, FormsModule, EditDinoComponent],
+  imports: [CommonModule, DisplayDinoComponent, EditDinoComponent],
 })
 export class DetailsComponent {
   protected readonly detailsStore = inject(DetailsStoreService);
--- packages/angular-dinos/dino-ui/src/lib/components/display-dino/display-dino.component.ts
+++ packages/angular-dinos/dino-ui/src/lib/components/display-dino/display-dino.component.ts
@@ -1,12 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { DetailsStoreService } from '@fullstack-dinos/angular-dinos/dinos-gql';
 
 @Component({
   selector: 'fullstack-dinos-display-dino',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>display-dino works!</p>`,
+  template: `<h1 class="mb-1 text-blue-500">
+      {{ detailsStore.dinosaur().name }}
+    </h1>
+    <div class="columns-2">
+      <div class="text-2xl italic text-blue-500/70">
+        {{ detailsStore.genusSpecies() }}
+      </div>
+      <div class="text-right text-blue-500/70 italic">
+        <strong>Last updated:</strong>
+        {{ detailsStore.dinosaur().updatedAt | date: 'medium' }}
+      </div>
+    </div>
+    <div
+      class="card card-compact glass mt-4 bg-blue-900 text-blue-50 w-full shadow-lg"
+    >
+      <div class="card-body">
+        <h4 class="card-title">Description</h4>
+        <p>
+          <span *ngIf="detailsStore.dinosaur().description; let description">
+            {{ description }}
+          </span>
+          {{ detailsStore.dinosaur().name }} was around
+          {{ detailsStore.dinosaur().heightInMeters }} meters tall and weighed
+          around {{ detailsStore.dinosaur().weightInKilos | number }} kilograms.
+          {{ detailsStore.dinosaur().name }} did
+          {{ detailsStore.dinosaur().hasFeathers ? '' : 'not' }} have feathers.
+        </p>
+      </div>
+    </div>
+    <div
+      *ngIf="detailsStore.displayTrivia()"
+      class="card card-compact glass mt-4 w-full bg-primary text-primary-content shadow-lg"
+    >
+      <div class="card-body">
+        <h2 class="card-title">Trivia</h2>
+        <ul>
+          <li *ngFor="let item of detailsStore.dinosaur().trivia">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>`,
   styleUrls: ['./display-dino.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'block',
+  },
 })
-export class DisplayDinoComponent {}
+export class DisplayDinoComponent {
+  protected readonly detailsStore = inject(DetailsStoreService);
+}
