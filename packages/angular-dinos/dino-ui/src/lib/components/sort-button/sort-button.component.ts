f5b75e151c8e7710343f00059396ab591ff58bc9
xocomil <xocomil@gmail.com>
2023-10-01 12:32:34 -0600

    convert to `@if()` block

--- packages/angular-dinos/dino-ui/src/lib/components/sort-button/sort-button.component.ts
+++ packages/angular-dinos/dino-ui/src/lib/components/sort-button/sort-button.component.ts
@@ -10,8 +10,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     class="btn btn-xs"
     (click)="sortClicked($event)"
   >
+    @if(direction === 'desc') { @defer (on immediate) {
     <svg
-      *ngIf="direction === 'desc'; else ascIcon"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -25,22 +25,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
         d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
       />
     </svg>
-    <ng-template #ascIcon>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-3 h-3"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-        />
-      </svg>
-    </ng-template>
+    } @placeholder { . } } @else { @defer (on immediate) {
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-3 h-3"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+      />
+    </svg>
+    } @placeholder { . } }
   </button>`,
   styleUrls: ['./sort-button.component.scss'],
   host: {
