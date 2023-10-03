574663e9ba788cd023c0515e4f44601836757d94
xocomil <xocomil@gmail.com>
2023-10-02 19:42:41 -0600

    use `&nbsp;` for our placeholder

--- packages/angular-dinos/dino-ui/src/lib/components/sort-button/sort-button.component.ts
+++ packages/angular-dinos/dino-ui/src/lib/components/sort-button/sort-button.component.ts
@@ -25,7 +25,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
         d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
       />
     </svg>
-    } @placeholder { . } } @else { @defer (on immediate) {
+    } @placeholder { &nbsp; } } @else { @defer (on immediate) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -40,7 +40,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
         d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
       />
     </svg>
-    } @placeholder { . } }
+    } @placeholder { &nbsp; } }
   </button>`,
   styleUrls: ['./sort-button.component.scss'],
   host: {
