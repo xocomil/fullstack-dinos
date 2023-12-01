3734884829ec449bf86d49e987ad64e9cab429e7
xocomil <xocomil@gmail.com>
2023-11-30 19:40:09 -0700

    finish converting to SignalStore

--- packages/angular-dinos/dinos-gql/src/lib/details.store.ts
+++ packages/angular-dinos/dinos-gql/src/lib/details.store.ts
@@ -1,4 +1,5 @@
 import { computed, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import {
   patchState,
   signalStore,
@@ -8,17 +9,24 @@ import {
 } from '@ngrx/signals';
 import { DinosCrudService } from './dinos-crud.service';
 import { emptyState } from './models/details.state';
-import { Dinosaur, validateUpdateDino } from './models/dinosaur';
+import {
+  Dinosaur,
+  errorParser,
+  validateDino,
+  validateUpdateDino,
+} from './models/dinosaur';
 
 export const DetailsStore = signalStore(
   withState(emptyState()),
-  withComputed(({ dinosaur, errors }) => ({
-    genusSpecies: computed(() => `${dinosaur.genus()} ${dinosaur.species()}`),
-    displayTrivia: computed(() => dinosaur.trivia().length),
+  withComputed(({ dinosaur, errors, savePending }) => ({
     errorsArray: computed(() => Object.values(errors())),
+    displayTrivia: computed(() => dinosaur.trivia().length),
+    genusSpecies: computed(() => `${dinosaur.genus()} ${dinosaur.species()}`),
+    showSaveSpinner: computed(() => savePending()),
   })),
   withMethods((state) => {
     const dinosCrudService = inject(DinosCrudService);
+    const router = inject(Router);
 
     return {
       setEditMode: (editMode: boolean | undefined) => {
@@ -41,14 +49,100 @@ export const DetailsStore = signalStore(
           console.error('dino error', dino.error);
         }
       },
-      updateDino: (dino: Dinosaur) => {
+      updateDino: async (dino: Dinosaur) => {
         const errors = validateUpdateDino(dino);
 
         console.log('errors', errors);
 
         patchState(state, { errors });
 
-        console.log('success');
+        if (Object.keys(errors).length) {
+          return;
+        }
+        console.log('Saving dino...', dino);
+
+        console.log('Current state', state.dinosaur().id);
+
+        const id = state.dinosaur().id;
+
+        if (!id) {
+          console.error('No id found for dino', dino);
+          return;
+        }
+
+        const result = await dinosCrudService.updateDinoPromise(dino, id);
+
+        console.log('save result', result);
+
+        patchState(state, { savePending: result.pending });
+
+        if (!result.finalized) {
+          return;
+        }
+
+        if (result.hasValue) {
+          router.navigate(['dinos']);
+
+          return;
+        }
+
+        if (result.hasError) {
+          const errorWithMessage = errorParser.safeParse(result.error);
+
+          if (errorWithMessage.success) {
+            patchState(state, {
+              networkError: errorWithMessage.data.message,
+            });
+
+            return;
+          }
+
+          patchState(state, { networkError: `Unknown error: ${result.error}` });
+        }
+      },
+      clearErrors: () => {
+        patchState(state, { errors: {} });
+      },
+      createDino: async (dino: Dinosaur) => {
+        const errors = validateDino(dino);
+
+        console.log('errors', errors);
+
+        patchState(state, { errors });
+
+        if (Object.keys(errors).length) {
+          return;
+        }
+
+        console.log('Saving dino...', dino);
+
+        const result = await dinosCrudService.createDinoPromise(dino);
+
+        patchState(state, { savePending: result.pending });
+
+        if (!result.finalized) {
+          return;
+        }
+
+        console.log('result', result);
+
+        if (result.hasValue) {
+          router.navigate(['dinos']);
+        }
+
+        if (result.hasError) {
+          const errorWithMessage = errorParser.safeParse(result.error);
+
+          if (errorWithMessage.success) {
+            patchState(state, {
+              networkError: errorWithMessage.data.message,
+            });
+
+            return;
+          }
+
+          patchState(state, { networkError: `Unknown error: ${result.error}` });
+        }
       },
     };
   }),
